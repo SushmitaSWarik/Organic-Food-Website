86a0c1a290a0f5c2851e664d09d13176efbbd5cb
Sushmita S Warik <sushmitaswarik@gmail.com>
2026-02-13 10:57:12 +0530

    Upadted Home Page design

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,19 @@
 let nav=document.querySelector(".navbar");
 window.onscroll = function(){
-    if(this.document.documentElement.scrollTop > 50){
+    if(document.documentElement.scrollTop > 50){
         nav.classList.add("header-scrolled")
     }
     else{
         nav.classList.remove("header-scrolled");
     }
 }
+
+// Nav Hide
+let navBar=document.querySelector(".nav-link");
+let navCollapse=document.querySelector(".navbar-collapse.collapse")
+navBar.forEach(function(e){
+    e.addEventListener("click", function(){
+        navCollapse.classList.remove("show"); //removing bootstrap class show
+    })
+})
+
